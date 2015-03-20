973fd9878b111c557afbf08b0da4d568aa7f4c9e
Dennis Brentjes <d.brentjes@gmail.com>
2015-03-20 20:09:15 +0100

    Merges local changes.

--- Cherry Pick/Cherry Pick.Windows/MainPage.xaml.cs
+++ Cherry Pick/Cherry Pick.Windows/MainPage.xaml.cs
@@ -5,13 +5,21 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
+using System.Diagnostics;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -25,6 +33,31 @@ namespace Cherry_Pick
         public MainPage()
         {
             this.InitializeComponent();
+            this.TakePicture();
+        }
+
+        public async void TakePicture()
+        {
+            MediaCapture captureManager = new MediaCapture();
+            await captureManager.InitializeAsync();
+            ImageEncodingProperties imgFormat = ImageEncodingProperties.CreateJpeg();
+
+            // create storage file in local app storage
+            try {
+                var file = await ApplicationData.Current.LocalFolder.CreateFileAsync(
+                    "TestPhoto.jpg",
+                    CreationCollisionOption.GenerateUniqueName
+                );
+                var stream = new InMemoryRandomAccessStream();
+                await captureManager.CapturePhotoToStreamAsync(imgFormat, stream);
+
+                byte[] b = new byte[stream.Size];
+                await stream.WriteAsync(b.AsBuffer());
+                
+            } catch (Exception e)
+            {
+                Debug.WriteLine(e.ToString());
+            }
         }
     }
 }
